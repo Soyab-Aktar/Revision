f973a9b102a56834de2a8b0d0f23efc62430c620
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-12-22 20:48:12 +0530

    New file

---
+++ Day 3/totalocc.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int solve1(vector<int> &arr, int size, int target)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+    int ans = 0;
+    while (start <= end)
+    {
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+
+    return ans;
+}
+
+int solve2(vector<int> &arr, int size, int target)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+    int ans = 0;
+    while (start <= end)
+    {
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr{10, 20, 30, 30, 30, 60, 60, 80, 90};
+    int size = arr.size();
+    int target;
+    cout << "Enter your target : ";
+    cin >> target;
+    int finalans1 = solve1(arr, size, target);
+    cout << "First occurence of an binery array at index no :" << finalans1 << endl;
+    int finalans2 = solve2(arr, size, target);
+    cout << "Last occurence of an binery array at index no :" << finalans2<<endl;
+    int final = finalans2 - finalans1 + 1;
+    cout << "Total occurence is : " << final;
+
+    return 0;
+}
